3e5bf332718f511f847e85af5e32bb1c70bab1a7
Moustafa Mahmoud <mustafaalaa.mohamed@gmail.com>
2025-02-07 11:13:38 +0000

    ADD lib

---
+++ frontend_app/lib/apiConstants.ts
@@ -0,0 +1,5 @@
+export const BASE_URL = "https://voiceapi.azurewebsites.net";
+export const REGISTER_API = `${BASE_URL}/register`;
+export const UPLOAD_API = `${BASE_URL}/upload_file`;
+export const JOBS_API = `${BASE_URL}/jobs`;
+
