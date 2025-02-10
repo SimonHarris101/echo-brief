f78689526f3cf4b806acd1c4a4e762b19fc30cdd
Moustafa Mahmoud <mustafaalaa.mohamed@gmail.com>
2025-02-10 23:42:02 +0000

    Fix UI APIs name

--- frontend_app/constants/apiConstants.js
+++
@@ -1,6 +0,0 @@
-export const BASE_NAME = "voiceapi";
-export const BASE_URL = `https://${BASE_NAME}.azurewebsites.net`;
-export const REGISTER_API = `${BASE_URL}/register`;
-export const LOGIN_API = `${BASE_URL}/login`;
-export const UPLOAD_API = `${BASE_URL}/upload`;
-
