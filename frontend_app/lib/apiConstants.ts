8b87deb9790e9baf5d422481ddecbe5b90546b55
Moustafa Mahmoud <mustafaalaa.mohamed@gmail.com>
2025-02-10 23:43:12 +0000

    Fix UI APIs name

--- frontend_app/lib/apiConstants.ts
+++ frontend_app/lib/apiConstants.ts
@@ -1,4 +1,4 @@
-export const BASE_NAME = "tf-echo-brief-dev-echo-brief-backend-api-prn6";
+export const BASE_NAME = "BASE_NAME";
 export const BASE_URL = `https://${BASE_NAME}.azurewebsites.net`;
 export const REGISTER_API = `${BASE_URL}/register`;
 export const UPLOAD_API = `${BASE_URL}/upload_file`;
